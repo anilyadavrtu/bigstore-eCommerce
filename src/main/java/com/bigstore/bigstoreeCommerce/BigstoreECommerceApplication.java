7ec04db7b439c929721e46be917e000aafbd2fd2
anilyadavrtu <anilyadav.rtu@gmail.com>
2020-01-15 14:50:30 +0530

    project setup

---
+++ src/main/java/com/bigstore/bigstoreeCommerce/BigstoreECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstoreeCommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigstoreECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigstoreECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bigstore/bigstoreeCommerce/BigstoreECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigstore.bigstoreeCommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigstoreECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
